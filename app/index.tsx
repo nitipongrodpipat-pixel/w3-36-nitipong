8ab974e7ba612b7728153a727c1ea337797479fb
huh it feeling good <nitipong.rodpipat@e-tech.ac.th>
2026-02-14 14:24:16 +0700

    Add files via upload

---
+++ app/index.tsx
@@ -0,0 +1,42 @@
+import { Button, StyleSheet, Text, View } from "react-native";
+import { useRouter } from "expo-router";
+
+
+export default function App(){
+
+    const router = useRouter()
+
+    return(
+        <View style={Styles.container}>
+            <Text style={Styles.MainTitle}>หน้าบ้าน</Text>
+            <Button title="ไปหน้าสี่เหลืยมคางหมู" onPress={() => router.navigate('/square')}/>
+
+
+            
+            <Button title="ไปหน้าหาพี้นที่" onPress={() => router.navigate('/past')}/>
+
+        </View>
+
+        
+        
+    )
+}
+const Styles = StyleSheet.create({
+    container:{
+
+        flex:1,
+        backgroundColor:"lightsalmon",
+        justifyContent:"center",
+        alignItems:"center"
+
+    },
+    MainTitle:{
+        fontSize:20,
+        fontWeight:"700"
+
+    }
+})
+
+
+
+
